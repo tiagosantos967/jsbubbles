1b08405028190cec734e5f048db4d68e90b55a4b
Tiago <tiago.s.1189@gmail.com>
2016-08-14 17:16:18 +0200

    removed animations for bubbles that have tooltips

--- src/js/jsbubbles.js
+++ src/js/jsbubbles.js
@@ -46,6 +46,16 @@ function JSBubbles(params, animations){
     return bubbles;
   };
 
+  var removeAllAnimations = function(id){
+    //remote all classes containing "animation" and 'transition'
+    var classes =  $('#bubble'+id).attr("class").split(" ");
+    for(var i=0; i< classes.length ; i++){
+      //if there are classes for anmations or transitions, remove them
+      if(~classes[i].indexOf('transition') || ~classes[i].indexOf('animation'))
+        $('#bubble'+id).removeClass(classes[i]);
+    }
+  };
+
   var startAnimation = function(id , animation_name , time , delay ){
     setTimeout(function() {
       $(id).addClass(animation_name);
@@ -76,6 +86,7 @@ function JSBubbles(params, animations){
       //register hover animation
       if(typeof animations.hover !== 'undefined'){
         if(~animations.hover.name.indexOf('transition')){
+          //not really starting but adding
           startAnimation('#bubble'+bubble,animations.hover.name,-1,0);
         } else {
           $('#bubble'+bubble).bind('mouseenter', {bubble: bubble, animations: animations} ,function(event) {
@@ -92,7 +103,7 @@ function JSBubbles(params, animations){
     console.log('JSBubbles loaded!');
 
     //complete given params and send them for rendering
-    renderParams(readParams( this.params ), this.animations);
+    this.addBubbles(this.params,this.animations);
   }.bind(this));
 
   //add notification bubble
@@ -101,10 +112,30 @@ function JSBubbles(params, animations){
       $('#bubble'+id).append("<div class='notification'>"+value+"</div>");
     else
       $('#bubble'+id+' > div.notification').html(value);
-  }
+  };
 
   this.removeNotifications =function(id){
     $('#bubble'+id).empty();
+  };
+
+  //adds bubbles to the end of the list
+  this.addBubbles =function(params,animations){
+    //complete given params and send them for rendering
+    renderParams(readParams( params ), animations);
+  };
+
+  //remove bubble from id
+  this.removeBubble= function(id){
+    $('#bubble'+id).remove();
+  };
+
+  this.setTooltipText = function(id,text){
+    removeAllAnimations(id);
+
+    if( $('#bubble'+id+' > div.tooltip').length == 0)
+      $('#bubble'+id).append("<div class='tooltip'>"+text+"</div>");
+    else
+      $('#bubble'+id+' > div.tooltip').html(text);
   }
 
 }
